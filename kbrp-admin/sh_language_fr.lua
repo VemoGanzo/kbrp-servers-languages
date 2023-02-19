e65abd32705f1ff318d9d169dbab58edb7bf828a
MrDupont <49952679+mimidu39@users.noreply.github.com>
2023-02-19 19:17:43 +0100

    Update sh_language_fr.lua

--- kbrp-admin/sh_language_fr.lua
+++ kbrp-admin/sh_language_fr.lua
@@ -7,7 +7,7 @@ DarkRP.RegisterSentences("kbrp_admin", "fr", {
 	["insultTicketReason"] = "Insultes",
 	["trollTicketReason"] = "Troll",
 	["freekillTicketReason"] = "Freekill",
-	["questionTicketReason"] = "Questions",
+	["questionTicketReason"] = "Question",
 	["lowGravity"] = "Basse",
 	["mediumGravity"] = "Moyenne",
 	["highGravity"] = "Haute",

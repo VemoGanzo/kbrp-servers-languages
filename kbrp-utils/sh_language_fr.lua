1319821baa69ba2c3fe1c525d2d03ea4e030a308
Bobninou0001 <75052612+Bobninou0001@users.noreply.github.com>
2023-02-19 14:11:33 -0500

    Update sh_language_fr.lua
    Change \n for a %s

--- kbrp-utils/sh_language_fr.lua
+++ kbrp-utils/sh_language_fr.lua
@@ -1,5 +1,5 @@
 DarkRP.RegisterSentences("kbrp_utils", "fr", {
-    ["cantSpawnTextScreenWithInsults"] = "Vous ne pouvez pas mettre ce textscreen car il contient le mot \n qui est interdit",
+    ["cantSpawnTextScreenWithInsults"] = "Vous ne pouvez pas mettre ce textscreen car il contient le mot %s qui est interdit",
 	["vipBypassJobLimit"] = "Grace à votre VIP, vous venez d'outrepasser la limite de joueurs dans le métier !",
 	["reachJobLimit"] = "La limite de joueurs a été atteinte !",
 	["staffSalary"] = "Votre salaire a été multiplié par deux car vous êtes actuellement en service !",

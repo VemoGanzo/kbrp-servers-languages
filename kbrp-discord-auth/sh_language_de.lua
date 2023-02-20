a4913c9693073aa085f0af9e984eeed01f5d7f1d
Luma <110888354+FoELuma@users.noreply.github.com>
2023-02-20 21:07:03 +0100

    german translation for discordauth

---
+++ kbrp-discord-auth/sh_language_de.lua
@@ -0,0 +1,4 @@
+DarkRP.RegisterSentences("kbrp_discord_auth", "de", {
+    ["receiveDiscordValidation"] = "Du hast gerade eine Discord Bestätigung erhalten um uns beizutreten!",
+	["discordUserAlreadyConnected"] = "Ein Discord-Nutzer ist bereits mit Ihrem Steam-Konto angemeldet."
+})

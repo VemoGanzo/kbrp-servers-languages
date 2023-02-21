5c696a983c8b1b2c435813abf79d1853696b2c88
Luma <110888354+FoELuma@users.noreply.github.com>
2023-02-21 23:18:52 +0100

    german translation for wallet

---
+++ kbrp-wallet/sh_language_de.lua
@@ -0,0 +1,5 @@
+DarkRP.RegisterSentences("kbrp_wallet", "de", {
+    ["frameTitle"] = "Portemonnaie",
+    ["dropMoneyButton"] = "Geld fallenlassen",
+    ["textEntryPlaceholder"] = "Geld fallenlassen (Du hast %s)"
+})

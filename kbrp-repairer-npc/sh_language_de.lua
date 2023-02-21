faf4596ab2adafb7cb65f69888b71dc9b6c2ad6a
Luma <110888354+FoELuma@users.noreply.github.com>
2023-02-21 21:32:41 +0100

    german translation for repairer-npc

---
+++ kbrp-repairer-npc/sh_language_de.lua
@@ -0,0 +1,16 @@
+DarkRP.RegisterSentences("kbrp_repairer_npc", "de", {
+    ["npcTitle"] = "ADAC",
+	["noVehicleToRepair"] = "Kein Fahrzeug in meiner Nähe benötigt eine Reparatur.",
+	["repairButton"] = "Reparieren für ",
+	["vehicleRepairingInstructionTitle"] = "Fahrzeugreparatur",
+	["vehicleRepairingInstructionDescription"] = "Ich repariere dein Fahrzeug für Geld!",
+	["invalidJob"] = "Mit diesem Beruf darfst du dein Fahrzeug nicht reparieren!",
+	["alreadyMechanic"] = "Es gibt einen ADAC in der Stadt, bitte wende dich an ihn.",
+	["invalidPermissions"] = "Du kannst dieses Fahrzeug nicht reparieren!",
+	["cantRepairTwiceVehicles"] = "Du darfst nicht mehr als 1 Fahrzeug reparieren!",
+	["cantAffordRepair"] = "Du hast nicht genug Geld für diese Reparatur!",
+	["repairInProgress"] = "Dein Fahrzeug wird repariert, bitte warte einen Moment...",
+	["npcTooFarRepairCanceled"] = "Du bist zu weit vom NPC entfernt, die Reparatur wird abgebrochen.",
+	["vehicleTooFarNPC"] = "Das Fahrzeug ist zu weit vom NPC entfernt, die Reparatur wird abgebrochen.",
+	["vehicleRepaired"] = "Dein Fahrzeug wurde erfolgreich repariert!"
+})

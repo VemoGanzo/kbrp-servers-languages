95da82ecfc2a0277b215fe0e3283a36e6404af37
Luma <110888354+FoELuma@users.noreply.github.com>
2023-02-21 23:15:27 +0100

    german translation for trunk

---
+++ kbrp-trunk/sh_language_de.lua
@@ -0,0 +1,16 @@
+DarkRP.RegisterSentences("kbrp_trunk", "de", {
+    ["inventoryFrameTitle"] = "Dein Inventar",
+	["dropItemOption"] = "Fallenlassen",
+	["useItemOption"] = "Benutzen",
+	["vehicleTrunkFrameTitle"] = "Kofferraum",
+	["errorOccured"] = "Es ist ein Fehler aufgetreten, bitte versuche es erneut!",
+	["trunkTooFar"] = "Du musst näher am Kofferaum sein!",
+	["invalidItem"] = "Dieses Item existiert nicht",
+	["errorOccured2"] = "Beim Hinzufügen dieses Items in deinen Kofferraum ist ein Fehler aufgetreten! (#1)",
+	["vehicleBlacklisted"] = "Du kannst keine Items in dieses Fahrzeug legen!",
+	["itemBlacklisted"] = "Du kannst dieses Item nicht in den Kofferraum legen!",
+	["noEnoughTrunkPlace"] = "Es gibt nicht genug Platz im Kofferraum!",
+	["equipWeapon"] = "Du rüstest deine Waffe aus...",
+	["weaponEquiped"] = "Du hast deine Waffe ausgerüstet!",
+	["trunkLocked"] = "Dieser Kofferraum ist abgeschlossen."
+})

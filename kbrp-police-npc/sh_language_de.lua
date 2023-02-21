f17062665ffcdc1ac62b00f0dc5f7544816f1503
Luma <110888354+FoELuma@users.noreply.github.com>
2023-02-21 20:58:24 +0100

    german translation for police npc

---
+++ kbrp-police-npc/sh_language_de.lua
@@ -0,0 +1,8 @@
+DarkRP.RegisterSentences("kbrp_police_npc", "de", {
+    ["getMyEquipment"] = "Ausrüstung bekommen",
+	["useNPCCooldown"] = "Du musst %s Sekunden warten um den NSC wieder benutzen zu können.",
+	["invalidJob"] = "Du hast nicht den richtigen Beruf um auf den NSC zugreifen zu können.",
+	["supplyInProgress"] = "Du rüstest dich aus...",
+	["npcTooFar"] = "Du bist zu weit vom NPC entfernt!",
+	["equipmentReceived"] = "Du hast gerade deine Ausrüstung erhalten!"
+})

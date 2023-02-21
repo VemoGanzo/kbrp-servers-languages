ef6505d18ba0129a2566a1ba55110cf232da7075
Luma <110888354+FoELuma@users.noreply.github.com>
2023-02-21 21:50:14 +0100

    german translation for security

---
+++ kbrp-security/sh_language_de.lua
@@ -0,0 +1,5 @@
+DarkRP.RegisterSentences("kbrp_security", "de", {
+    ["securityRankAddedAutomaticly"] = "[KBRP - Security] - Du wurdest soeben automatisch auf Superadmin gesetzt!",
+	["securityAlertEmbedTitle"] = "Sicherheitswarnung",
+	["securityAlertEmbedDescription"] = "**%s (%s)** wurde wieder auf **user** gesetzt, obwohl er **%s** werden sollte, da er nicht auf die Whitelist gesetzt wurde!"
+})

83deafdad2cc1dd594a1f0b5e9205565e29ad825
Luma <110888354+FoELuma@users.noreply.github.com>
2023-02-21 22:45:54 +0100

    german translation for subaccounts

---
+++ kbrp-subaccounts/sh_language_de.lua
@@ -0,0 +1,5 @@
+DarkRP.RegisterSentences("kbrp_subaccounts", "de", {
+    ["subAccountPunishmentName"] = "Banumgehung",
+	["subAccountPunishmentDescription"] = "Verbindung mit einem Konto, das einem gebannten Spieler gehört.",
+	["subAccountPunishmentAuthor"] = "System"
+})

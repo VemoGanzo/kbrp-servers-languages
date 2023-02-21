01fb47c6e33310e7ec13e028b015d6ef4046605d
Luma <110888354+FoELuma@users.noreply.github.com>
2023-02-21 18:06:10 +0100

    german translation for health system

---
+++ kbrp-health/sh_language_de.lua
@@ -0,0 +1,10 @@
+DarkRP.RegisterSentences("kbrp_health", "de", {
+    ["npcTitle"] = "Chefarzt",
+	["alreadyMedic"] = "Lass mich in Ruhe! Es gibt einen Arzt in dieser Stadt, ich habe jetzt Urlaub!",
+	["noNeedToBeCared"] = "Keine Sorge, mit ihnen ist alles gut.",
+	["takeCareText"] = "Lasse dich für %s behandeln.",
+	["cantAffordCare"] = "Du kannst dir eine Behandlung nicht leisten.",
+	["careInProgress"] = "Du wirst behandelt...",
+	["npcTooFar"] = "Du bist zu weit vom Arzt entfernt! So können wir dich nicht behandeln.",
+	["beenCared"] = "Du wurdest erfolgreich behandelt!"
+})

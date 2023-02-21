14091f3f37cb31ad9ad67849f0295c918ec61a5b
Luma <110888354+FoELuma@users.noreply.github.com>
2023-02-21 21:13:03 +0100

    german translation for questions

---
+++ kbrp-questions/sh_language_de.lua
@@ -0,0 +1,10 @@
+DarkRP.RegisterSentences("kbrp_questions", "de", {
+    ["testFrameTitle"] = "Wissenstest",
+	["answersAmount"] = " (%s Antwort%s)",
+	["finishTestButton"] = "Meine Antwort bestätigen und den Test beenden.",
+	["nextQuestionButton"] = "Nächste Frage (%s/%s)",
+	["testPassedNotification"] = "Herzlichen Glückwunsch du hast den Test bestanden!",
+	["testFailedNotification"] = "Leider hast du den Test nicht bestanden. :(",
+	["invalidPlayTimeToPlayJob"] = "Du hast nicht genug Spielzeit um diesen Beruf zu spielen.",
+	["testCooldownNotification"] = "Du musst noch %s warten bevor du dein Glück wieder versuchen kannst."
+})

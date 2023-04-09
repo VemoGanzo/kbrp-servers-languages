172bdc66d48fdb357806603e3790491e54b48245
Emilca1 <94971475+Emilca1@users.noreply.github.com>
2023-04-09 15:39:04 +0200

    Improved kbrp-whitelist french translation

--- kbrp-whitelist/sh_language_fr.lua
+++ kbrp-whitelist/sh_language_fr.lua
@@ -1,6 +1,6 @@
 DarkRP.RegisterSentences("kbrp_whitelist", "fr", {
-    ["cantJoin"] = "Vous n'êtes pas sur la whitelist du serveur\npour plus d'information rendez-vous sur notre dashboard\nhttps://dashboard.kobralost-rp.fr/",
+    ["cantJoin"] = "Vous n'êtes pas sur la whitelist du serveur\npour plus d'informations rendez-vous sur notre dashboard\nhttps://dashboard.kobralost-rp.fr/",
     ["removeWhitelist"] = "%s à été supprimé de la whitelist du serveur",
     ["addWhitelist"] = "%s à été ajouté temporairement à la whitelist du serveur",
-    ["removedFromWhitelistKick"] = "Vous avez été expulsé du serveur car vous venez d'être retirer de la whitelist"
+    ["removedFromWhitelistKick"] = "Vous avez été expulsé du serveur car vous venez d'être retiré de la whitelist"
 })

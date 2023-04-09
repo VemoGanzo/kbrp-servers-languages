80b6431cdf5e7773c2d11928cd133335dbafd98d
Emilca1 <94971475+Emilca1@users.noreply.github.com>
2023-04-10 01:03:18 +0200

    Improver kbrp-trunk french translation

--- kbrp-trunk/sh_language_fr.lua
+++ kbrp-trunk/sh_language_fr.lua
@@ -12,6 +12,6 @@ DarkRP.RegisterSentences("kbrp_trunk", "fr", {
 	["noEnoughTrunkPlace"] = "Il n'y a pas suffisament de place dans le coffre !",
 	["equipWeapon"] = "Vous équipez votre arme...",
 	["weaponEquiped"] = "Vous venez d'équiper votre arme",
-	["trunkLocked"] = "Ce coffre de véhicule est vérouillé.",
-	["tooMuchFoodBag"] = "Votre véhicule ne peut pas contenir d'avantage de sac de nourriture !"
+	["trunkLocked"] = "Ce coffre de véhicule est verrouillé.",
+	["tooMuchFoodBag"] = "Votre véhicule ne peut pas contenir d'avantage de sacs de nourriture !"
 })
